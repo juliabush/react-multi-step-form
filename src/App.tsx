0d9770bd6ffccfb7a7df421d62736f9167b09027
juliabush <j.elizabethbush@gmail.com>
2025-06-07 14:33:33 +0200

    Adding global and local css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <form action=""></form>
+    <div className="container">
+      <form action="">
+        <div className="page-number"></div>
+      </form>
     </div>
   );
 }
