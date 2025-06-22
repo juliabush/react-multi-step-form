52ffd8f06de5b04f2da0ed5bba8be11a75b0dbc1
juliabush <j.elizabethbush@gmail.com>
2025-06-22 11:33:14 +0200

    Installed nodemon to watch js file for changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node API server!");
+  res.send("Hello from Node API server!!!");
 });
 
 app.listen(3000, () => {
