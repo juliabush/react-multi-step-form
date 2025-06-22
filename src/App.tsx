49731dc646947d16edc6db9882884d8bbda415c1
juliabush <j.elizabethbush@gmail.com>
2025-06-22 12:24:05 +0200

    Fixing button alignment between steps

--- src/App.tsx
+++ src/App.tsx
@@ -332,6 +332,11 @@ function Step5(): ReactElement {
         alt="Thank you illustration"
         className="thankyou-icon"
       />
+      <p>
+        Thanks for subscribing! We've sent a confirmation email to
+        test@email.com. We're excited to have you on board and hope you enjoy
+        using our platform!
+      </p>
     </>
   );
 }
