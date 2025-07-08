c89d74e372ec18486994f88b8d36580ab4093ab6
juliabush <j.elizabethbush@gmail.com>
2025-07-08 08:45:03 +0200

    adding cors middleware and defining post endpoint

--- server/index.js
+++ server/index.js
@@ -19,3 +19,13 @@ const transporter = nodemailer.createTransport({
 const app = express();
 // assigning constant port a value, 3000 is used as its a common default
 const port = 3000;
+// Important line, adds middleware that allows express to read req.body when sending JSON data
+app.use(express.json());
+// app.use tells express app to aplly middleware to every http request and cors is a middleware function, that allows webpages from one domain(origin) to request another domain
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+// app.post defines a route handler for http post requests, /sendmail is the url endpoint, sendMAil is a function called whenever post is made to endpoint
+app.post("/sendmail", sendMail);
